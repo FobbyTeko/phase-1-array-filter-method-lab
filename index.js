67825c81d549bcb5c9c42ff5ebdcc5de72b1334c
Tiri Tuatai <tiri.jnr@gmail.com>
2024-03-19 23:39:08 +0800

    filter method done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(array, string) {
+    const filteredDrivers = array.filter(function (driver){
+        if (driver.toLowerCase() === string.toLowerCase()){
+            return driver
+        }
+    });
+    return filteredDrivers
+}
+
+
+function fuzzyMatch(array, string) {
+    const filteredDrivers = array.filter(function (driver){
+        if (driver[0, 1] === string[0, 1]){
+            return driver
+        }
+    });
+    return filteredDrivers
+}
+
+function matchName (array, string) {
+    const filteredDrivers = array.filter(function (driver){
+        if (driver.name === string){
+            return driver
+        }
+    });
+    return filteredDrivers
+}
+
